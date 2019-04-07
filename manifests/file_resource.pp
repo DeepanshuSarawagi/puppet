2c60c9b208a4a9cbc920a661fcf83ef1610d103b
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2019-04-07 18:46:31 +0530

    Correct the puppet style code to get rid of puppet-lint warnings

--- manifests/file_resource.pp
+++ manifests/file_resource.pp
@@ -1,7 +1,9 @@
 file { '/tmp/file_resource':
-  ensure => present,
-  owner => 'root',
-  group => 'root',
+  ensure  => present,
+  owner   => 'root',
+  group   => 'root',
   content => "This is my first manifest and it is managed by puppet. Please dont edit it/n",
-  mode => '0644',
+  #lint:ignore:double_quoted_strings
+  mode    => '0644',
+  #lint:endignore
 }
