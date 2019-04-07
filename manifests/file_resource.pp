38701a4c850c2b8b94026c349405b0067abc39cc
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2019-04-06 22:20:09 -0700

    Correct the syntax error

--- manifests/file_resource.pp
+++ manifests/file_resource.pp
@@ -1,7 +1,7 @@
 file { '/tmp/file_resource':
 	ensure => present,
-	owner => root,
-	group => root,
+	owner => 'root',
+	group => 'root',
 	content => "This is my first manifest and it is managed by puppet. Please dont edit it/n",
-	mode => 0644,
+	mode => '0644',
 }
