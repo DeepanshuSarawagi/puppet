04ada7a91f3070110c834f041aff8575da948372
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2019-04-10 18:18:07 +0530

    Understanding various attributes of exec resource

---
+++ manifests/exec_refreshonly.pp
@@ -0,0 +1,10 @@
+file { '/tmp/dir':
+  ensure => directory,
+  notify => Exec['newdir'],
+}
+
+exec { 'newdir':
+  command     => 'mkdir -p /tmp/dir',
+  refreshonly => true,
+  logoutput   => true,
+}
